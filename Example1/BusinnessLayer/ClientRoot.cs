4ce08cdfa1c32c3f3d4c9e9ae55962f6df2008d4
Robert HC <bj112143@gmail.com>
2019-07-19 20:58:49 -0500

    First Example

---
+++ Example1/BusinnessLayer/ClientRoot.cs
@@ -0,0 +1,33 @@
+﻿using Csla;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinnessLayer
+{
+    [Serializable]
+    public class ClientRoot : BusinessBase<ClientRoot>
+    {
+        public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(c => c.Name);
+
+        [Required]
+        public string Name
+        {
+            get { return GetProperty(NameProperty); }
+            set { SetProperty(NameProperty, value); }
+        }
+
+        public static readonly PropertyInfo<string> EmailProperty = RegisterProperty<string>(c => c.Email);
+
+        [StringLength(15)]
+        public string Email
+        {
+            get { return GetProperty(EmailProperty); }
+            set { SetProperty(EmailProperty, value); }
+        }
+
+        public static ClientRoot NewEditableClientRoot()
+        {
+            return DataPortal.Create<ClientRoot>();
+        }
+    }
+}
---
+++ Example1/Example1/Example1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using BusinnessLayer;
+using Csla.Rules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Example1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private ClientRoot _clientRoot;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+            DatePicker.MaximumDate = DateTime.Today;
+            DatePicker.MinimumDate = DateTime.Today.AddMonths(-1);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _clientRoot = ClientRoot.NewEditableClientRoot();
+                _clientRoot.Name = TxtEmail.Text;
+                _clientRoot.Email = TxtEmail.Text;
+
+                _clientRoot = _clientRoot.Save();
+
+                Indicator.IsRunning = true;
+            }
+            catch(ValidationException)
+            {
+                DisplayAlert("Validation Error", string.Join("\n", _clientRoot.BrokenRulesCollection.ToList()), "Cancel");
+            }
+        }
+    }
+}
